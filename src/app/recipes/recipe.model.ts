2159a012e099b6b2a031c76ab9e7e1a5005977d1
Ariel Gianatiempo <agianatiempo@tecnosoftware.com>
2018-06-29 17:42:05 -0300

    lecture 54

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-  name: string;
-  description: string;
-  imagePath: string;
-
-  constructor(name: string, desc: string, image: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = image;
-  }
+  constructor(public name: string, public description: string, public imagePath: string) { }
 }
