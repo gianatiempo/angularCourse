01d7990c4716c30a5dd95ad4b7dd2d1e3e23ce39
Ariel Gianatiempo <agianatiempo@tecnosoftware.com>
2018-06-29 17:30:14 -0300

    lecture 49bis

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('recipe1', 'recipe1 description','https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg'),
-    new Recipe('recipe1', 'recipe1 description','https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
+    new Recipe('recipe1', 'recipe1 description',
+    'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg'),
+    new Recipe('recipe1', 'recipe1 description',
+    'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
   ];
 
   constructor() { }
